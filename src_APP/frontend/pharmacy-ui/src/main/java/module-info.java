40baeab80acfc8ed234378711ab867cf430203a2
Khoa Huynh <khoahuynh2106@gmail.com>
2026-05-18 13:13:17 +0700

    fix merge conflict module-info.java

--- src_APP/frontend/pharmacy-ui/src/main/java/module-info.java
+++ src_APP/frontend/pharmacy-ui/src/main/java/module-info.java
@@ -4,14 +4,11 @@ module com.pharmacy {
     requires transitive javafx.graphics;
     requires java.net.http;
     requires transitive javafx.base;
-<<<<<<< HEAD
     requires com.google.gson;
     // Mở folder dto cho Gson dùng reflection để gán dữ liệu
     opens com.pharmacy.dto to com.google.gson;
 
-=======
     requires com.fasterxml.jackson.databind;
->>>>>>> d0d91036713e81c30bf5e178ecc766bae107245c
     // Mở tất cả package chứa Controller cho JavaFX FXMLLoader
     opens com.pharmacy.controller.admin to javafx.fxml, javafx.base;
     opens com.pharmacy.controller.common to javafx.fxml;   // nếu có
