2a23c1ec3d69ebc81b45a6006563438c32fd2292
nvanphat_184 <nguyenvanphat150506@gmail.com>
2026-05-19 20:56:52 +0700

    module-info.java + prcedures.sql(chua save)

--- src_APP/frontend/pharmacy-ui/src/main/java/module-info.java
+++ src_APP/frontend/pharmacy-ui/src/main/java/module-info.java
@@ -4,35 +4,24 @@ module com.pharmacy {
     requires transitive javafx.graphics;
     requires java.net.http;
     requires transitive javafx.base;
-<<<<<<< HEAD
-    
-    // Thêm Jackson để xử lý JSON
-=======
     requires com.google.gson;
-    // Mở folder dto cho Gson dùng reflection để gán dữ liệu
     opens com.pharmacy.dto to com.google.gson;
 
->>>>>>> 24b6ea243e8ac44b2ee5eb222524b197f838a8a6
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.annotation;
 
     // Mở tất cả package chứa Controller cho JavaFX FXMLLoader
-<<<<<<< HEAD
-    opens com.pharmacy.controller.admin to javafx.fxml;
-    opens com.pharmacy.controller.common to javafx.fxml;
-=======
     opens com.pharmacy.controller.admin to javafx.fxml, javafx.base;
     opens com.pharmacy.controller.common to javafx.fxml;   // nếu có
 
     // Mở package chứa LoginController (vì nó cũng là controller)
->>>>>>> 24b6ea243e8ac44b2ee5eb222524b197f838a8a6
     opens com.pharmacy to javafx.fxml;
     opens com.pharmacy.controller.warehouse;
     opens com.pharmacy.controller.sales to javafx.fxml;
 
     // Mở các package chứa DTO và Model để Jackson có thể truy cập (Reflection)
-    opens com.pharmacy.dto to com.fasterxml.jackson.databind;
+    
     opens com.pharmacy.model to com.fasterxml.jackson.databind;
 
     // Export các package cần thiết
